baf738fd89431a8c9ded6b9aa195216bdcb23eda
ASPIRE7\lucia <luciabortolozzi@gmail.com>
2024-11-14 18:11:57 +0100

    [FEATURE][003]: Create and integrate a custom Bootstrap theme

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">LuShop</Navbar.Brand>
+          <Navbar.Brand><img src={logo} alt="logo"/>LuShop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
