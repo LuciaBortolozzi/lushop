1ed3adf897c29d58de85b154784b7978a877a799
ASPIRE7\lucia <luciabortolozzi@gmail.com>
2024-11-26 00:23:48 +0100

    [FIX][012]: Fix module issue

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import products from "./data/products.js";
 
 dotenv.config();
-
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -20,4 +19,6 @@ app.get("/", (_req, res) => {
   res.send("API is running...");
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
